7207ec2b9d2dfce4ded70488bae3c89efca79ee0
Denis <skadenis@mail.ru>
2019-01-30 23:17:30 +0300

    Time

--- meal-prep/src/components/Orders.vue
+++ meal-prep/src/components/Orders.vue
@@ -90,8 +90,7 @@ export default {
       this.$store.dispatch("getCurrentOrder", id);
     },
     convertTime(time){
-        alert(time);
-      return moment(time).add(-1 * Date.getTimezoneOffset(),'m').format('LT')
+      return moment(time).add(-1 * new Date().getTimezoneOffset(),'m').format('LT')
     }
   }
 };
